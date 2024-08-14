0591eb234c12e527e07c944d36cd21e395ae46b0
dawood-faiz-tvs <dawood.faiz.tvs@gmail.com>
2024-08-14 19:00:52 +0500

    learned about how can we separate validation logic from business logic along with how to make HTTP requests with Accept header

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\APILoginRequest;
 use App\Traits\APIResponses;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     use APIResponses;
-    public function login()
+    public function login(APILoginRequest $request)
     {
-        return $this->ok('HELLO LOGIN!');
+        return $this->ok($request->get('email'));
+    }
+
+    public function register()
+    {
+        return $this->ok('Register here please...');
     }
 }
