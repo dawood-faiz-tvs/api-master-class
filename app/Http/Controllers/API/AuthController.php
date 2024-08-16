932f6026328a27ca46b849c1e68dadf90093feeb
mfiazahmad1 <102288621+dawood-faiz-tvs@users.noreply.github.com>
2024-08-17 01:30:29 +0500

    learned about how to authenticate a user and generate API tokens

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Traits\APIResponses;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\API\LoginUserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    use APIResponses;
+    public function login(LoginUserRequest $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return $this->error('Invalid Credentials', 401);
+        }
+
+        $user = User::firstWhere('email', $request->email);
+
+        return $this->ok('Authenticated', [
+            'token' => $user->createToken('API token for user: ' . $request->email)->plainTextToken
+        ]);
+    }
+
+    public function register() {}
+}
--- routes/api-v1.php
+++ routes/api-v1.php
@@ -5,8 +5,4 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::apiResource('tickets', TicketController::class);
+Route::apiResource('tickets', TicketController::class)->middleware('auth:sanctum');
