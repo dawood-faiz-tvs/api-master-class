34edd532500f8c7aeeb2d28b5108a846fdad2270
mfiazahmad1 <102288621+dawood-faiz-tvs@users.noreply.github.com>
2024-08-18 14:12:05 +0500

    learned about how we can omit and add data conditionally using JSON resource

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\API\V1\TicketController;
+use App\Http\Controllers\API\V1\UserController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('tickets', TicketController::class)->middleware('auth:sanctum');
+Route::apiResource('users', UserController::class)->middleware('auth:sanctum');
